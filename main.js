2d69f45ce1ef8e6a79aa23d5d82f456d32fd9f5f
Tim01Bro <arthaos14@gmail.com>
2019-10-20 15:50:15 +0300

    init commit

---
+++ main.js
@@ -0,0 +1,20 @@
+var songs = ["BillieEilish-BadGuy.mp3","BillieEilish-Copycat.mp3","BillieEilish-BuryAFriend.mp3"];
+var postersSong = ["poster1.jpg","poster2.jpg","poster3.jpg"];
+var currentSong = 0;
+
+var songName = document.getElementById("songTitle");
+var fill = document.getElementById("fill");
+
+var song = new Audio();
+
+window.onload = playSong;
+
+function playSong(){
+    song.src = songs[currentSong];
+    
+    songName.textContent = songs[currentSong];
+
+    song.play();
+ }
+
+ 
